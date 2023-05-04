09ff4cc0c308b524b920f43ea329ceb3b6c3a269
Andres Navarro <101758804+FelipeCarrr@users.noreply.github.com>
2023-05-05 00:31:34 +0200

    Primer commit del backend

---
+++ BackEnd/la-cosecha/src/main/java/com/app/lacosecha/LaCosechaApplication.java
@@ -0,0 +1,13 @@
+package com.app.lacosecha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaCosechaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaCosechaApplication.class, args);
+	}
+
+}
---
+++ BackEnd/la-cosecha/src/test/java/com/app/lacosecha/LaCosechaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.lacosecha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaCosechaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
